97b2754c17bae52f552d30592283165568104397
PJain7988 <priyajain7988@gmail.com>
2026-07-02 15:48:06 +0530

    Refactor Tic Tac Toe with Object-Oriented Design and proper input validation

--- Tic_Tac_Toe/Tic_Tac_Toe.cpp
+++ Tic_Tac_Toe/Tic_Tac_Toe.cpp
@@ -1,166 +1,135 @@
-#include<bits/stdc++.h>
+#include <iostream>
+#include <vector>
+#include <string>
+#include <limits>
+
 using namespace std;
-string Winner(char Ttt[][3], string P1, string P2)
-{
-    string Winner = " ";
-    if ((Ttt[0][0] == 'X' && Ttt[0][1] == 'X' && Ttt[0][2] == 'X') ||
-        (Ttt[1][0] == 'X' && Ttt[1][1] == 'X' && Ttt[1][2] == 'X') ||
-        (Ttt[2][0] == 'X' && Ttt[2][1] == 'X' && Ttt[2][2] == 'X') ||
-        (Ttt[0][0] == 'X' && Ttt[1][0] == 'X' && Ttt[2][0] == 'X') ||
-        (Ttt[0][1] == 'X' && Ttt[1][1] == 'X' && Ttt[2][1] == 'X') ||
-        (Ttt[0][2] == 'X' && Ttt[1][2] == 'X' && Ttt[2][2] == 'X') ||
-        (Ttt[0][0] == 'X' && Ttt[1][1] == 'X' && Ttt[2][2] == 'X') ||
-        (Ttt[0][2] == 'X' && Ttt[1][1] == 'X' && Ttt[2][0] == 'X')){
-        Winner = P1;
-    }
-    if ((Ttt[0][0] == 'O' && Ttt[0][1] == 'O' && Ttt[0][2] == 'O') ||
-        (Ttt[1][0] == 'O' && Ttt[1][1] == 'O' && Ttt[1][2] == 'O') ||
-        (Ttt[2][0] == 'O' && Ttt[2][1] == 'O' && Ttt[2][2] == 'O') ||
-        (Ttt[0][0] == 'O' && Ttt[1][0] == 'O' && Ttt[2][0] == 'O') ||
-        (Ttt[0][1] == 'O' && Ttt[1][1] == 'O' && Ttt[2][1] == 'O') ||
-        (Ttt[0][2] == 'O' && Ttt[1][2] == 'O' && Ttt[2][2] == 'O') ||
-        (Ttt[0][0] == 'O' && Ttt[1][1] == 'O' && Ttt[2][2] == 'O') ||
-        (Ttt[0][2] == 'O' && Ttt[1][1] == 'O' && Ttt[2][0] == 'O')){
-        Winner = P2;
-    }
-    return Winner;
-}
-string PlayerInput(char start[3][3] , string P1 , string P2){
-    string p1;
-    string p2;
-    char repeat[10];
-    string Win;
-    for (int i = 0; i < 5; i++){
-        cout << P1 << " turn : ";
-        do{
-            cin >> p1;
-            if (p1.length() == 1 && p1[0]>48 && p1[0]<58 && p1[0] != repeat[0] && p1[0] != repeat[1]
-                && p1[0] != repeat[2] && p1[0] != repeat[3] && p1[0] != repeat[4] && p1[0] != repeat[5]
-                && p1[0] != repeat[6] && p1[0] != repeat[7] && p1[0] != repeat[8] && p1[0] != repeat[9]){
-                cout << endl;
-                break;
-            }
-            else if (p1[0] == repeat[0] || p1[0] == repeat[1] || p1[0] == repeat[2] || p1[0] == repeat[3]
-                || p1[0] == repeat[4] || p1[0] == repeat[5] || p1[0] == repeat[6] || p1[0] == repeat[7]
-                || p1[0] == repeat[8] || p1[0] == repeat[9])
-            {
-                cout << "\n * *This position is occupied.Please choose an another position. * *\n\n";
-                cout << P1 << " turn again : ";
-            }
-            else
-            {
-                cout << "\nInvalid input.\nPlease enter a natural number from the range 1 to 9.\n\n";
-                cout << P1 << " turn again : ";
-            }
-        } while (true);
 
-        if (p1[0] == '1') { start[0][0] = 'X'; }
-        if (p1[0] == '2') { start[0][1] = 'X'; }
-        if (p1[0] == '3') { start[0][2] = 'X'; }
-        if (p1[0] == '4') { start[1][0] = 'X'; }
-        if (p1[0] == '5') { start[1][1] = 'X'; }
-        if (p1[0] == '6') { start[1][2] = 'X'; }
-        if (p1[0] == '7') { start[2][0] = 'X'; }
-        if (p1[0] == '8') { start[2][1] = 'X'; }
-        if (p1[0] == '9') { start[2][2] = 'X'; }
- 
-        Win = Winner(start, P1, P2);
- 
-        cout << " " << start[0][0] << " | " << start[0][1] << " | " << start[0][2];
-        cout << "\t\t 1 | 2 | 3 " << endl;
-        cout << " " << start[1][0] << " | " << start[1][1] << " | " << start[1][2] ;
-        cout << "\t\t 4 | 5 | 6 " << endl;
-        cout << " " << start[2][0] << " | " << start[2][1] << " | " << start[2][2] ;
-        cout << "\t\t 7 | 8 | 9 " << endl << endl;
- 
-        if (Win == P1 || Win == P2) break;
-        else if (i == 4) { Win = "Draw"; break; }
-        repeat[i] = p1[0];
-        cout << P2 << " turn : ";
-        do
-        {
-            cin >> p2;
-            if (p2.length() == 1 && p2[0]>48 && p2[0]<58 && p2[0] != p1[0] && p2[0] != repeat[0] &&
-                p2[0] != repeat[1] && p2[0] != repeat[2] && p2[0] != repeat[3] && p2[0] != repeat[4]
-                && p2[0] != repeat[5] && p2[0] != repeat[6] && p2[0] != repeat[7] && p2[0] != repeat[8]
-                && p2[0] != repeat[9])
-            {
-                cout << endl;
-                break;
-            }
- 
-            else if (p2[0] == repeat[0] || p2[0] == repeat[1] || p2[0] == repeat[2] || p2[0] == repeat[3]
-                || p2[0] == repeat[4] || p2[0] == repeat[5] || p2[0] == repeat[6] || p2[0] == repeat[7]
-                || p2[0] == repeat[8] || p2[0] == repeat[9])
-                 {
-                cout << "\n * *This position is occupied.Please choose an another position. * *\n\n";
-                cout << P2 << " turn again : ";
-                 }
-            else
-            {
-                cout << "\nInvalid input.\nPlease enter a natural number from the range 1 to 9.\n\n";
-                cout << P2 << " turn again : ";
-            }
-        } while (true);
- 
- 
-        if (p2[0] == '1') { start[0][0] = 'O'; }
-        if (p2[0] == '2') { start[0][1] = 'O'; }
-        if (p2[0] == '3') { start[0][2] = 'O'; }
-        if (p2[0] == '4') { start[1][0] = 'O'; }
-        if (p2[0] == '5') { start[1][1] = 'O'; }
-        if (p2[0] == '6') { start[1][2] = 'O'; }
-        if (p2[0] == '7') { start[2][0] = 'O'; }
-        if (p2[0] == '8') { start[2][1] = 'O'; }
-        if (p2[0] == '9') { start[2][2] = 'O'; }
- 
-        Win = Winner(start, P1, P2);
-  
-        cout << " " << start[0][0] << " | " << start[0][1] << " | " << start[0][2];
-        cout << "\t\t 1 | 2 | 3 " << endl;
-        cout << " " << start[1][0] << " | " << start[1][1] << " | " << start[1][2];
-        cout << "\t\t 4 | 5 | 6 " << endl;
-        cout << " " << start[2][0] << " | " << start[2][1] << " | " << start[2][2];
-        cout << "\t\t 7 | 8 | 9 " << endl << endl;
- 
- 
-        if (Win == P1 || Win == P2) break; 
-        repeat[i + 5] = p2[0];
-    }
-    return Win;
-}
-void Display(string W , string p1 , string p2){
-    if (W == p1 || W == p2)
-    {
-        cout << "\n\n\t\t\t\t\t        Winner of Game is : " <<W << endl << endl;
-    }
-    else cout << "\t\t\t\t\t\t\tGame is Draw \n";
-    cout << "\t\t\t\t\t *** Thank You for using our Game Program ***\n";
-    cout << "\t\t\t\t_ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ \n\n";
-}
-int main()
-{
-    char start[3][3]{
-       {'-', '-', '-'},
-       {'-', '-', '-'},
-       {'-', '-', '-'}
-    };
-    string P1;
-    string P2;
-    cout << "\t\t\t\t     ***  TIC - TAC - TOE  ***\n\n";
-    cout << "\t\t\t\t        Welcome to our Game\n\n";
+class TicTacToe {
+private:
+    char board[3][3];
+    string player1;
+    string player2;
+    char currentPlayerSymbol;
+    string currentPlayerName;
+    int movesCount;
 
-    cout << "Enter player 1 name : ";
-    getline(cin, P1);
-    cout << "Enter player 2 name : ";
-    getline(cin, P2);
+    void initializeBoard() {
+        for (int i = 0; i < 3; ++i) {
+            for (int j = 0; j < 3; ++j) {
+                board[i][j] = '1' + (i * 3 + j);
+            }
+        }
+    }
 
-    cout << "\nChoose following format to enter input\n\n";
-    cout << " 1 | 2 | 3 " << endl;
-    cout << " 4 | 5 | 6 " << endl;
-    cout << " 7 | 8 | 9 " << endl << endl;
- 
-    string Winner = PlayerInput(start, P1, P2);
-    Display(Winner, P1, P2);
+    void displayBoard() const {
+        cout << "\n";
+        cout << "     |     |     \n";
+        cout << "  " << board[0][0] << "  |  " << board[0][1] << "  |  " << board[0][2] << "  \n";
+        cout << "_____|_____|_____\n";
+        cout << "     |     |     \n";
+        cout << "  " << board[1][0] << "  |  " << board[1][1] << "  |  " << board[1][2] << "  \n";
+        cout << "_____|_____|_____\n";
+        cout << "     |     |     \n";
+        cout << "  " << board[2][0] << "  |  " << board[2][1] << "  |  " << board[2][2] << "  \n";
+        cout << "     |     |     \n\n";
+    }
+
+    bool checkWin() const {
+        // Check rows and columns
+        for (int i = 0; i < 3; ++i) {
+            if (board[i][0] == board[i][1] && board[i][1] == board[i][2]) return true;
+            if (board[0][i] == board[1][i] && board[1][i] == board[2][i]) return true;
+        }
+        // Check diagonals
+        if (board[0][0] == board[1][1] && board[1][1] == board[2][2]) return true;
+        if (board[0][2] == board[1][1] && board[1][1] == board[2][0]) return true;
+        
+        return false;
+    }
+
+    void clearInputBuffer() {
+        cin.clear();
+        cin.ignore(numeric_limits<streamsize>::max(), '\n');
+    }
+
+public:
+    TicTacToe() : movesCount(0) {
+        initializeBoard();
+    }
+
+    void setupPlayers() {
+        cout << "Enter Player 1 name (X): ";
+        getline(cin, player1);
+        if (player1.empty()) player1 = "Player 1";
+        
+        cout << "Enter Player 2 name (O): ";
+        getline(cin, player2);
+        if (player2.empty()) player2 = "Player 2";
+
+        currentPlayerName = player1;
+        currentPlayerSymbol = 'X';
+    }
+
+    void switchPlayer() {
+        if (currentPlayerSymbol == 'X') {
+            currentPlayerSymbol = 'O';
+            currentPlayerName = player2;
+        } else {
+            currentPlayerSymbol = 'X';
+            currentPlayerName = player1;
+        }
+    }
+
+    void playGame() {
+        cout << "\n==========================================================" << endl;
+        cout << "                 *** TIC-TAC-TOE ***" << endl;
+        cout << "==========================================================" << endl;
+        setupPlayers();
+
+        while (movesCount < 9) {
+            displayBoard();
+            cout << currentPlayerName << "'s turn (" << currentPlayerSymbol << "). ";
+            cout << "Enter the position (1-9): ";
+
+            int choice;
+            if (!(cin >> choice) || choice < 1 || choice > 9) {
+                cout << "Invalid input. Please enter a number between 1 and 9." << endl;
+                clearInputBuffer();
+                continue;
+            }
+
+            int row = (choice - 1) / 3;
+            int col = (choice - 1) % 3;
+
+            if (board[row][col] == 'X' || board[row][col] == 'O') {
+                cout << "Position already occupied! Choose another." << endl;
+                continue;
+            }
+
+            board[row][col] = currentPlayerSymbol;
+            movesCount++;
+
+            if (checkWin()) {
+                displayBoard();
+                cout << "\n**********************************************************" << endl;
+                cout << "   Congratulations! " << currentPlayerName << " wins the game!" << endl;
+                cout << "**********************************************************\n" << endl;
+                return;
+            }
+
+            switchPlayer();
+        }
+
+        displayBoard();
+        cout << "\n**********************************************************" << endl;
+        cout << "                    It's a draw!" << endl;
+        cout << "**********************************************************\n" << endl;
+    }
+};
+
+int main() {
+    TicTacToe game;
+    game.playGame();
     return 0;
 }
